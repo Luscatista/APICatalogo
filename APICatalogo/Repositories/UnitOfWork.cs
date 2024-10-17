bb423c6f029fba16a6eb610eec8c8c328ac9370b
Lucas Baptista <lucas.baptista@live.com>
2024-10-17 11:50:48 -0300

    UnitOfWork para eliminar concorrencia de dados

--- APICatalogo/Repositories/UnitOfWork.cs
+++ APICatalogo/Repositories/UnitOfWork.cs
@@ -1,50 +1,46 @@
 ﻿using APICatalogo.Context;
 
-namespace APICatalogo.Repositories
+namespace APICatalogo.Repositories;
+
+public class UnitOfWork : IUnitOfWork
 {
-    public class UnitOfWork : IUnitOfWork
+    private IProdutoRepository? _produtoRepo;
+    private ICategoriaRepository? _categoriaRepo;
+
+    public AppDbContext _context;
+    public UnitOfWork(AppDbContext context)
     {
-        private IProdutoRepository _produtoRepo;
+        _context = context;
+    }
 
-        private ICategoriaRepository _categoriaRepo;
-
-        public AppDbContext _context;
-
-        public UnitOfWork(AppDbContext context)
+    public IProdutoRepository ProdutoRepository
+    {
+        get
         {
-            _context = context;
+            return _produtoRepo = _produtoRepo ?? new ProdutoRepository(_context);
+            //Acima é o mesmo que dizer :
+            //if(_produtoRepo is null)
+            //{
+            //    _produtoRepo = new ProdutoRepository(_context)
+            //}
+            //return _produtoRepo;
         }
+    }
 
-        public IProdutoRepository ProdutoRepository
+    public ICategoriaRepository CategoriaRepository
+    {
+        get
         {
-            get
-            {
-                return _produtoRepo = _produtoRepo ?? new ProdutoRepository(_context);
-                //Acima é o mesmo que dizer :
-                //if(_produtoRepo is null)
-                //{
-                //    _produtoRepo = new ProdutoRepository(_context)
-                //}
-                //return _produtoRepo;
-            }
+            return _categoriaRepo = _categoriaRepo ?? new CategoriaRepository(_context);
         }
+    }
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
 
-        public ICategoriaRepository CategoriaRepository
-        {
-            get
-            {
-                return _categoriaRepo = _categoriaRepo ?? new CategoriaRepository(_context);
-            }
-        }
-        public void Commit()
-        {
-            _context.SaveChanges();
-        }
-
-        public void Dispose()
-        {
-            _context.Dispose();
-        }
-
+    public void Dispose()
+    {
+        _context.Dispose();
     }
 }
